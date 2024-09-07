cc2002f561ac7e0a0b7ab134198a8530de1b1ecb
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-09-07 21:22:19 +0530

    Add : toastNotifiContainer.jsx

---
+++ src/toastnotifiContainer/ToastNotifiContainer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ToastNotifiContainer = () => {
+    return(
+        <>
+        <div className="container">
+            <div className="button-toast-box">
+                <button>Sucess Toast</button>
+                <button>Info Toast</button>
+                <button>Warning Toast</button>
+                <button>Error Toast</button>
+            </div>
+        </div>
+        </>
+    )
+}
+export default ToastNotifiContainer;
