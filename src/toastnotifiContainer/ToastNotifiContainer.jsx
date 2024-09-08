cf973502241190143cfeae315e68f4f655e08ca3
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-09-08 14:52:23 +0530

    add : handler's to the button

--- src/toastnotifiContainer/ToastNotifiContainer.jsx
+++ src/toastnotifiContainer/ToastNotifiContainer.jsx
@@ -4,16 +4,25 @@ const ToastNotifiContainer = () => {
 
     const [showToast, setShowToast] = useState();
 
+    const handleToastAdd = () => {
+        setShowToast(true);
+        setTimeout(handleToastClose, 5000)
+    }
+
+    const handleToastClose = () => {
+        setShowToast(false)
+    }
+
     return (
         <>
             <div className="container">
                 <div className="toast-container">
                     {showToast && (<div className="toast">
-                        Success Toast <span onClick={() => { setShowToast(false) }}>x</span>
+                        Success Toast <span onClick={handleToastClose}>x</span>
                     </div>)}
                 </div>
                 <div className="button-toast-box">
-                    <button onClick={() => { setShowToast(true) }}>Success Toast</button>
+                    <button onClick={handleToastAdd}>Success Toast</button>
                     <button>Info Toast</button>
                     <button>Warning Toast</button>
                     <button>Error Toast</button>
