4890fae5bb701a82a4fef72b702c98c67728160f
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-09-09 21:32:43 +0530

    add : onClick handler & Condition in ClassName

--- src/Projectask2/StartRating/starRating.jsx
+++ src/Projectask2/StartRating/starRating.jsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import './rating.css';
 
-const StarRating = ({StarCount = 10})=>{
-    return(
+const StarRating = ({ StarCount = 10 }) => {
+    const [starColored, setStarColored] = useState(0);
+    console.log('intialState', starColored);
+    console.log('array', StarCount)
+    return (
         <>
-        <div className="container">
-            {new Array(StarCount).fill(0).map(()=>{
-                return(
-                    <span>&#9733;</span>
-                )
-            })}
-        </div>
+            <div className="container">
+                {new Array(StarCount).fill(0).map((index,_) => {
+                    return (
+                        <span key={index} 
+                        onClick={() => setStarColored(index + 1)}
+                         className={index < starColored ? "gold" : ""}>&#9733;</span>
+                    )
+                })}
+            </div>
         </>
     )
 }
