bc24540962016d596ba88c027a7edf9fe6085194
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-09-09 19:13:52 +0530

    add : StarRating.jsx ans rating.css file

---
+++ src/Projectask2/StartRating/starRating.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './rating.css';
+
+const StarRating = ()=>{
+    return(
+        <>
+        <h1>hey , this is star rating componnets</h1>
+        </>
+    )
+}
+export default StarRating;
