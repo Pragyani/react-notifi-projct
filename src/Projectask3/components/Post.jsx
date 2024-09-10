a280a2ba7fd9321d4423e637c50d859116105caf
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-09-10 19:08:48 +0530

    add : Post.jsx

---
+++ src/Projectask3/components/Post.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './pagination.css';
+import Pagination from "./Pagination";
+
+const Post = ()=>{
+     return(
+        <>
+        <h1>this is sa post components</h1>
+        <Pagination/>
+        </>
+     )
+}
+export default Post;
