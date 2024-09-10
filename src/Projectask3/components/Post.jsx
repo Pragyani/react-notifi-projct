721952fcc9aef0ffb06dba23d76218a6c3c636e7
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-09-10 21:08:26 +0530

    add : fetching data from api & updation in Post.jsx

--- src/Projectask3/components/Post.jsx
+++ src/Projectask3/components/Post.jsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './pagination.css';
 import Pagination from "./Pagination";
 
-const Post = ()=>{
-     return(
+const Post = () => {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    const getData = async (url) => {
+        try {
+            const response = await fetch('https://picsum.photos/v2/list?page=1&limit=5');
+            // console.log('data getig', response);
+            const data = await response.json();
+            // console.log(data);
+            setData(data);
+        } catch {
+            console.log('erorr found ')
+        }
+    }
+    return (
         <>
-        <h1>this is sa post components</h1>
-        <Pagination/>
+            <div className="container-p">
+                <div className="post-container">
+                    <img src="https://picsum.photos/id/30/1280/901" alt="pgition-imge" />
+                </div>
+                <Pagination />
+            </div>
+
         </>
-     )
+    )
 }
 export default Post;
