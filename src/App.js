31c097c4cd7ae0fd58058be805af654ea23cb5f6
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-09-10 19:10:02 +0530

    add : render's Post components in the App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import Post from './Projectask3/components/Post';
 // import ToastNotifiContainer from './Projectask1/toastnotifiContainer/ToastNotifiContainer';
-import StarRating from './Projectask2/StartRating/starRating';
+// import StarRating from './Projectask2/StartRating/starRating';
 
 function App() {
   return (
     <>
       {/* <ToastNotifiContainer /> */}
-      <StarRating />
+      {/* <StarRating /> */}
+      <Post/>
     </>
   );
 }
